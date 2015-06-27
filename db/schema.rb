632038bffcdbeb2edb38458b4d0e21ef8c976f05
Robert <robertlplee@gmail.com>
2015-06-27 19:07:01 +0200

    added menu_items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627165559) do
+ActiveRecord::Schema.define(version: 20150627170616) do
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string   "item"
+    t.float    "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string   "dish"
